d622a836313fa780c997e8f2c5ae7688c980170c
mateusz.zarkowski@gmail.com <>
2014-08-05 22:55:33 +0200

    Start!

---
+++ SatansCock/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: Gambit
+ *
+ * Created on 05 August 2014, 22:53
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    cout << "Szatan " << endl;
+    return 0;
+}
+
